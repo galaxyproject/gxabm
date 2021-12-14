9f1e1069c0b7e792155ca1c6b69ffd359a9d27a1
Keith Suderman <suderman@jhu.edu>
2021-12-14 10:04:48 -0500

    Wrap wait_for_job in try/except block

--- lib/workflow.py
+++ lib/workflow.py
@@ -391,16 +391,20 @@ def wait_for_jobs(gi: GalaxyInstance, invocations: dict):
         job_id = step['job_id']
         if job_id is not None:
             print(f"Waiting for job {job_id} on {lib.GALAXY_SERVER}")
-            status = gi.jobs.wait_for_job(job_id, 86400, 10, False)
-            data = gi.jobs.show_job(job_id, full_details=True)
-            metrics = {
-                'workflow_id': wfid,
-                'history_id': hid,
-                'metrics': data,
-                'status': status,
-                'server': lib.GALAXY_SERVER
-            }
-            output_path = os.path.join(METRICS_DIR, f"{job_id}.json")
-            with open(output_path, "w") as f:
-                json.dump(metrics, f, indent=4)
-                print(f"Wrote metrics to {output_path}")
+            try:
+                # TDOD Should retry if anything throws an exception.
+                status = gi.jobs.wait_for_job(job_id, 86400, 10, False)
+                data = gi.jobs.show_job(job_id, full_details=True)
+                metrics = {
+                    'workflow_id': wfid,
+                    'history_id': hid,
+                    'metrics': data,
+                    'status': status,
+                    'server': lib.GALAXY_SERVER
+                }
+                output_path = os.path.join(METRICS_DIR, f"{job_id}.json")
+                with open(output_path, "w") as f:
+                    json.dump(metrics, f, indent=4)
+                    print(f"Wrote metrics to {output_path}")
+            except Exception as e:
+                print(f"ERROR: {e}")
