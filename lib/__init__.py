6809fd03e53436eb259a6bd901afc22d90acd339
Keith Suderman <suderman@jhu.edu>
2021-12-22 14:24:46 -0800

    Add globals and Keys class

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,3 +4,18 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 # TODO: These should be encapsultated into a proper *context* type object.
 global GALAXY_SERVER, API_KEY, KUBECONFIG
 GALAXY_SERVER = API_KEY = KUBECONFIG = None
+INVOCATIONS_DIR = "invocations"
+METRICS_DIR = "metrics"
+
+
+class Keys:
+    NAME = 'name'
+    RUNS = 'runs'
+    INPUTS = 'inputs'
+    REFERENCE_DATA = 'reference_data'
+    WORKFLOW_ID = 'workflow_id'
+    DATASET_ID = 'dataset_id'
+    HISTORY_BASE_NAME = 'output_history_base_name'
+    HISTORY_NAME = 'history_name'
+
+
