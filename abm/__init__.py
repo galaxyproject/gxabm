0fe884f97e5ff88147ca78c9cdfd90f50016e704
Keith Suderman <suderman@jhu.edu>
2022-01-26 13:46:21 -0500

    Read version number from a file

--- abm/__init__.py
+++ abm/__init__.py
@@ -1,8 +1,6 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-#__version__="2.0.0-dev6"
-
 def getVersion():
     dir = os.path.dirname(os.path.realpath(__file__))
     version_file = os.path.join(dir, 'VERSION')
