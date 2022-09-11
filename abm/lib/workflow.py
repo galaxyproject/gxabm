1682bc59c315d902d1dc8f1e1c3c05a731955205
Keith Suderman <suderman@jhu.edu>
2022-09-11 16:08:18 -0400

    Upload workflows from URLs

--- abm/lib/workflow.py
+++ abm/lib/workflow.py
@@ -3,9 +3,13 @@ import json
 import logging
 
 from pprint import pprint
+
+import requests
+import yaml
 from planemo.runnable import for_path
 from planemo.galaxy.workflows import install_shed_repos
 from common import connect, Context
+from pathlib import Path
 
 log = logging.getLogger('abm')
 
@@ -33,6 +37,9 @@ def upload(context: Context, args: list):
         print('ERROR: no workflow file given')
         return
     path = args[0]
+    if path.startsWith('http'):
+        import_from_url(context, args)
+        return
     if not os.path.exists(path):
         print(f'ERROR: file not found: {path}')
         return
@@ -43,6 +50,49 @@ def upload(context: Context, args: list):
     print("Installing tools")
     result = install_shed_repos(runnable, gi, False)
     pprint(result)
+
+
+def import_from_url(context: Context, args:list):
+    if len(args) == 0:
+        print("ERROR: no workflow URL given")
+        return
+    url = args[0]
+    response = requests.get(url)
+    if (response.status_code != 200):
+        print(f"ERROR: There was a problem downloading the workflow: {response.status_code}")
+        print(response.reason)
+        return
+    try:
+        workflow = json.loads(response.text)
+    except Exception as e:
+        print("ERROR: Unable to parse workflow")
+        print(e)
+        return
+
+    gi = connect(context)
+    gi.workflows.import_workflow_dict(workflow)
+    print(f"Imported workflow from {url}")
+
+
+def import_from_config(context: Context, args:list):
+    if len(args) == 0:
+        print("ERROR: no workflow URL given")
+        return
+    key = args[0]
+    userfile = os.path.join(Path.home(), ".abm", "workflows.yml")
+    if not os.path.exists(userfile):
+        print("ERROR: this instance has not been configured to import workflows.")
+        print(f"Please configure {userfile} to enable workflow imports")
+        return
+    with open(userfile, 'r') as f:
+        workflows = yaml.safe_load(f)
+    pprint(workflows)
+    if not key in workflows:
+        print(f"ERROR: no such workflow: {key}")
+        return
+
+    url = workflows[key]
+    import_from_url(context, [ url ])
 
 
 def download(context: Context, args: list):
