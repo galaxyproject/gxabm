f4803778d385c6f1a15f2e796cc5e06ca2ed5c76
Keith Suderman <suderman@jhu.edu>
2022-09-25 09:40:25 -0400

    Better error handling if menu handler not found

--- abm/lib/cloudlaunch.py
+++ abm/lib/cloudlaunch.py
@@ -5,14 +5,14 @@ import yaml
 from pathlib import Path
 from common import print_json, print_yaml
 
-def list(context: Context, args: list[str]):
-    pass
+def list(context: Context, args: list):
+    print("TBD: list deployments")
 
 
-def create(context: Context, args: list[str]):
-    pass
+def create(context: Context, args: list):
+    print("TBD: create a deployment")
 
 
-def delete(context: Context, args: list[str]):
-    pass
+def delete(context: Context, args: list):
+    print("TBD: delete a deployment")
 
