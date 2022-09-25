9089d1b066e79e939ed7fce844160999764f4194
Keith Suderman <suderman@jhu.edu>
2022-09-25 08:20:24 -0400

    Add skeleton for cloudlaunch

---
+++ abm/lib/cloudlaunch.py
@@ -0,0 +1,18 @@
+from common import Context
+
+import os
+import yaml
+from pathlib import Path
+from common import print_json, print_yaml
+
+def list(context: Context, args: list[str]):
+    pass
+
+
+def create(context: Context, args: list[str]):
+    pass
+
+
+def delete(context: Context, args: list[str]):
+    pass
+
